bda104fc13ed9cb3a46c578ba699ae2aad9894ca
docbrown85 <me@example.com>
2020-04-23 17:56:30 +0200

    add Number Wizard Console source code

---
+++ number_wizard_console/Assets/NumberWizard.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NumberWizard : MonoBehaviour
+{
+    int max;
+    int min;
+    int guess;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartGame();
+    }
+
+    void StartGame()
+    {
+        max = 1000;
+        min = 1;
+        guess = 500;
+
+        Debug.Log("Welcome to the Number Wizard!");
+        Debug.Log("Pick a number, but do not tell me what it is...");
+        Debug.Log("The highest number you can pick is: " + max);
+        Debug.Log("The lowest number you can pick is:" + min);
+        Debug.Log("The lowest number you can pick is:" + min);
+        Debug.Log("Tell me if your number is higher than my guess:" + guess);
+        Debug.Log("Push Up = Higher, Push Down = Lower, Push Enter = Correct");
+
+        max = max + 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            min = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            max = guess;
+            NextGuess();
+        }
+        else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Debug.Log("Got it right!");
+            StartGame();
+        }
+
+    }
+
+    void NextGuess()
+    {
+        guess = (min + max) / 2;
+        Debug.Log("Is it higher or lower than: " + guess);
+    }
+}
